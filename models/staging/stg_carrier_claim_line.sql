5bd5538ccfa4eaa57354593e5d870edfcf6b4ee8
Rob Hughes <rghughes94@gmail.com>
2025-03-20 23:24:51 +0000

    updating carrier files

--- models/staging/stg_carrier_claim_line.sql
+++ models/staging/stg_carrier_claim_line.sql
@@ -1,73 +1,73 @@
 select
-      desy_sort_key
-    , claim_no
-    , line_num
-    , clm_thru_dt
-    , nch_clm_type_cd
-    , carr_prfrng_pin_num
-    , prf_physn_upin
-    , prf_physn_npi
-    , org_npi_num
-    , carr_line_prvdr_type_cd
-    , prvdr_state_cd
-    , prvdr_spclty
-    , prtcptng_ind_cd
-    , carr_line_rdcd_pmt_phys_astn_c
-    , line_srvc_cnt
-    , line_cms_type_srvc_cd
-    , line_place_of_srvc_cd
-    , carr_line_prcng_lclty_cd
-    , line_last_expns_dt
-    , hcpcs_cd
-    , hcpcs_1st_mdfr_cd
-    , hcpcs_2nd_mdfr_cd
-    , betos_cd
-    , line_nch_pmt_amt
-    , line_bene_pmt_amt
-    , line_prvdr_pmt_amt
-    , line_bene_ptb_ddctbl_amt
-    , line_bene_prmry_pyr_cd
-    , line_bene_prmry_pyr_pd_amt
-    , line_coinsrnc_amt
-    , line_sbmtd_chrg_amt
-    , line_alowd_chrg_amt
-    , line_prcsg_ind_cd
-    , line_pmt_80_100_cd
-    , line_service_deductible
-    , carr_line_mtus_cnt
-    , carr_line_mtus_cd
-    , line_icd_dgns_cd
-    , line_icd_dgns_vrsn_cd
-    , line_hct_hgb_rslt_num
-    , line_hct_hgb_type_cd
-    , line_ndc_cd
-    , carr_line_clia_lab_num
-    , carr_line_ansthsa_unit_cnt
-    , carr_line_cl_chrg_amt
-    , line_othr_apld_ind_cd1
-    , line_othr_apld_ind_cd2
-    , line_othr_apld_ind_cd3
-    , line_othr_apld_ind_cd4
-    , line_othr_apld_ind_cd5
-    , line_othr_apld_ind_cd6
-    , line_othr_apld_ind_cd7
-    , line_othr_apld_amt1
-    , line_othr_apld_amt2
-    , line_othr_apld_amt3
-    , line_othr_apld_amt4
-    , line_othr_apld_amt5
-    , line_othr_apld_amt6
-    , line_othr_apld_amt7
-    , thrpy_cap_ind_cd1
-    , thrpy_cap_ind_cd2
-    , thrpy_cap_ind_cd3
-    , thrpy_cap_ind_cd4
-    , thrpy_cap_ind_cd5
-    , clm_next_gnrtn_aco_ind_cd1
-    , clm_next_gnrtn_aco_ind_cd2
-    , clm_next_gnrtn_aco_ind_cd3
-    , clm_next_gnrtn_aco_ind_cd4
-    , clm_next_gnrtn_aco_ind_cd5
-    , file_name
-    , ingest_datetime
+      cast(desy_sort_key as {{ dbt.type_string() }} ) as 
+    , cast(claim_no as {{ dbt.type_string() }} ) as 
+    , cast(line_num as integer) as 
+    , {{ try_to_cast_date('c.clm_thru_dt', 'YYYYMMDD') }} as 
+    , cast(nch_clm_type_cd as {{ dbt.type_string() }} ) as 
+    , cast(carr_prfrng_pin_num as {{ dbt.type_string() }} ) as 
+    , cast(prf_physn_upin as {{ dbt.type_string() }} ) as 
+    , cast(prf_physn_npi as {{ dbt.type_string() }} ) as 
+    , cast(org_npi_num as {{ dbt.type_string() }} ) as 
+    , cast(carr_line_prvdr_type_cd as {{ dbt.type_string() }} ) as 
+    , cast(prvdr_state_cd as {{ dbt.type_string() }} ) as 
+    , cast(prvdr_spclty as {{ dbt.type_string() }} ) as 
+    , cast(prtcptng_ind_cd as {{ dbt.type_string() }} ) as 
+    , cast(carr_line_rdcd_pmt_phys_astn_c as {{ dbt.type_string() }} ) as 
+    , cast(regexp_substr(line_srvc_cnt,'.') as integer) as
+    , cast(line_cms_type_srvc_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_place_of_srvc_cd as {{ dbt.type_string() }} ) as 
+    , cast(carr_line_prcng_lclty_cd as {{ dbt.type_string() }} ) as 
+    , {{ try_to_cast_date('line_last_expns_dt', 'YYYYMMDD') }} as 
+    , cast(hcpcs_cd as {{ dbt.type_string() }} ) as 
+    , cast(hcpcs_1st_mdfr_cd as {{ dbt.type_string() }} ) as 
+    , cast(hcpcs_2nd_mdfr_cd as {{ dbt.type_string() }} ) as 
+    , cast(betos_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_nch_pmt_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_bene_pmt_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_prvdr_pmt_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_bene_ptb_ddctbl_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_bene_prmry_pyr_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_bene_prmry_pyr_pd_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_coinsrnc_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_sbmtd_chrg_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_alowd_chrg_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_prcsg_ind_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_pmt_80_100_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_service_deductible as {{ dbt.type_numeric() }} ) as 
+    , cast(carr_line_mtus_cnt as {{ dbt.type_numeric() }} ) as 
+    , cast(carr_line_mtus_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_icd_dgns_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_icd_dgns_vrsn_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_hct_hgb_rslt_num as {{ dbt.type_string() }} ) as 
+    , cast(line_hct_hgb_type_cd as {{ dbt.type_string() }} ) as 
+    , cast(line_ndc_cd as {{ dbt.type_string() }} ) as 
+    , cast(carr_line_clia_lab_num as {{ dbt.type_string() }} ) as 
+    , cast(carr_line_ansthsa_unit_cnt as {{ dbt.type_numeric() }} ) as 
+    , cast(carr_line_cl_chrg_amt as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_ind_cd1 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd2 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd3 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd4 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd5 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd6 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_ind_cd7 as {{ dbt.type_string() }} ) as 
+    , cast(line_othr_apld_amt1 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt2 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt3 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt4 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt5 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt6 as {{ dbt.type_numeric() }} ) as 
+    , cast(line_othr_apld_amt7 as {{ dbt.type_numeric() }} ) as 
+    , cast(thrpy_cap_ind_cd1 as {{ dbt.type_string() }} ) as 
+    , cast(thrpy_cap_ind_cd2 as {{ dbt.type_string() }} ) as 
+    , cast(thrpy_cap_ind_cd3 as {{ dbt.type_string() }} ) as 
+    , cast(thrpy_cap_ind_cd4 as {{ dbt.type_string() }} ) as 
+    , cast(thrpy_cap_ind_cd5 as {{ dbt.type_string() }} ) as 
+    , cast(clm_next_gnrtn_aco_ind_cd1 as {{ dbt.type_string() }} ) as 
+    , cast(clm_next_gnrtn_aco_ind_cd2 as {{ dbt.type_string() }} ) as 
+    , cast(clm_next_gnrtn_aco_ind_cd3 as {{ dbt.type_string() }} ) as 
+    , cast(clm_next_gnrtn_aco_ind_cd4 as {{ dbt.type_string() }} ) as 
+    , cast(clm_next_gnrtn_aco_ind_cd5 as {{ dbt.type_string() }} ) as 
+    , cast(file_name as {{ dbt.type_string() }} ) as 
+    , cast(ingest_datetime as {{ dbt.type_timestamp() }} ) as ingest_datetime 
 from {{ source('medicare_lds','carrier_claim_line') }}
